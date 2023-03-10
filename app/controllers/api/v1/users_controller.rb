bcd4ef7da3bc6805722a1abcc070555cfdcd45b0
DataNick <DataNick@users.noreply.github.com>
2023-03-10 18:42:16 -0500

    Remove test index action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,14 +1,27 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authenticate, only: [:create, :index]
 
-  def index
-    render json: {status: :ok, message: 'hello world'}
-  end
-
   def create
-    # create user, hash password, and save to redis store
+    if params[:username].nil? || params[:password].nil?
+      render json: {errors: "bad request", status_code: 400}, status: :bad_request
+      return
+    end
+    # username, password = params[:username], params[:password]
+    user = User.new(user_params)
+    if user.save_to_redis_store
+      render json: user.serializable_hash, status: 200
+    else
+     render json: { message: "Bad credentials", status: :unauthorized, code: 401 }, status: :unauthorized
+    end
   end
 
   def verify
+    render json: { message: "Welcome.", status: :ok, code: 200 }, status: :ok
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
