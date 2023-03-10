1a6fc58cad056b7c9cc548b8f9d021c41f7480fd
DataNick <DataNick@users.noreply.github.com>
2023-03-10 18:41:29 -0500

    Use cookies to store encrypted username between requests

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,13 +1,39 @@
 class Api::V1::AuthenticationController < ApplicationController
-  skip_before_action :authenticate_user
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
 
-  # POST /api/v1/auth/login
+  # before_action :authenticate
+  skip_before_action :authenticate
+
   def login
-    #find user from redis store
-    # check if user is authenticated
-    #if authenticated, set token and time expiration
-    # render json with token, expiration, username and status ok
-    # if not authenticated, return unauthorized error with status unauthorized
+    username, password = params[:username], params[:password]
+    redis_search = $redis_credentials.get(params[:username])
+    if redis_search.nil?
+      render json: { message: "Record not found", status: :not_found, code: 404 }, status: :not_found
+    else
+      password_digest = redis_search
+      user = User.new(username:username, password_digest: password_digest)
+      if user.authenticate(password)
+        encrypted_data = encrypt(username)
+        cookies[:username] = encrypted_data
+        cookies[:salt] = response
+        render json: {message: 'Successfully logged in.'}, status: 200
+      else
+        render json: { message: "Bad credentials", status: :unauthorized, code: 401 }, status: :unauthorized
+      end
+    end
+  end
+
+#
+#   "username": "Dan",
+# "password_digest": "$2a$12$5il2V34yTJZtRDp5vf391epzChiWFqhgN6t6LXC9XOlb6kVmOkuiG",
+# "password": "Pa33&w0rD"
+# 6323819
+
+  def logout
+    cookies[:username] = ''
+    cookies[:hash] = ''
+    render json: {message: 'Successfully logged out.'}, status: 200
   end
 
 end
