122b2fac97253739798d39b6c5fd46cefc8aa4f0
DataNick <DataNick@users.noreply.github.com>
2023-03-10 18:42:55 -0500

    Clean up applicaion controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,33 +1,28 @@
 class ApplicationController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
+  include ActionController::Cookies
   rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
-
   before_action :authenticate
 
   private
-
-  # def authenticate
-  #   authenticate_user_with_token || handle_bad_authentication
-  # end
 
   def authenticate
     authenticate_user || handle_bad_authentication
   end
 
-  def authenticate_user_with_token
-    authenticate_with_http_token do |token, options|
-      @user ||= User.find_by(private_api_key: token)
+  def authenticate_user
+    # using username but should be built out to substitute with a JWT or Auth token
+    username = decrypt(cookies[:username])
+    response = $redis_credentials.get(username)
+    if !response.nil?
+      return true
+    else
+      return false
     end
   end
 
-  def authenticate_user
-     # @user ||= User.find_by(private_api_key: token)
-     # grab user with username and password, dehash password to confirm correct credentials
-
-  end
-
   def handle_bad_authentication
-    render json: { message: "Bad credentials", status: :unauhorized, code: 401 }, status: :unauthorized
+    render json: { message: "Bad credentials", status: :unauthorized, code: 401 }, status: :unauthorized
   end
 
   def handle_not_found
