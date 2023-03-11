abe93862db67b89de3702619b6a60ef83112d585
DataNick <DataNick@users.noreply.github.com>
2023-03-11 11:16:28 -0500

    Move save and find methods for redis datastore to separate module

---
+++ app/models/concerns/redis_record.rb
@@ -0,0 +1,21 @@
+module RedisRecord
+  
+  def save_to_redis_store
+    if self.valid? && unique_username(self.username)
+      $redis_credentials.set(self.username, self.password_digest)
+      return self
+    else
+      return false
+    end
+  end
+
+  def find_by_username(username)
+    password_digest = $redis_credentials.get(username)
+    if password_digest.nil?
+      return nil
+    else
+      self.new(username:username, password_digest:password_digest)
+    end
+  end
+
+end
