06225a26832f4ccabae8ecac1643146c0e721862
DataNick <DataNick@users.noreply.github.com>
2023-03-11 14:01:37 -0500

    Update tests

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
   include EncryptionHelper
   describe 'user#create' do
-    it 'create returns http success' do
-      if $redis_credentials.get('testuser')
-        $redis_credentials.del('testuser')
+    it 'returns http success with valid credentials' do
+      if $redis_credentials.get('testuser1')
+        $redis_credentials.del('testuser1')
       end
-      post :create, as: :json, params: {username: 'TestUser', password: 'Pa33w$0rd'}
+      post :create, as: :json, params: {username: 'TestUser1', password: 'Pa33w$0rd'}
       expect(response).to have_http_status(:success)
     end
 
     it 'ensures only unique usernames can be stored in datastore' do
-      post :create, as: :json, params: {username: 'TestUser', password: 'Pa33w$0rd'}
+      post :create, as: :json, params: {username: 'TestUser1', password: 'Pa33w$0rd'}
       expect(response).to have_http_status(:unauthorized)
     end
 
     it 'ensures passwords are in a valid format when signing up' do
-      post :create, as: :json, params: {username: 'TestUser', password: 'pa33w0rd'}
+      post :create, as: :json, params: {username: 'TestUser1', password: 'pa33w0rd'}
       expect(response).to have_http_status(:unauthorized)
     end
 
-    it 'create returns 400 if credentials are not passed' do
+    it 'returns 400 if credentials are not passed' do
       post :create, as: :json, params: {username: '', password: 'Pa33w$0rd'}
       expect(response).to have_http_status(:unauthorized)
     end
@@ -29,7 +29,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
   describe 'user#auth_verification' do
     it 'returns 200 status if user is authenticated' do
-      cookies[:username] = encrypt('testuser')
+      cookies[:username] = encrypt('testuser1')
       get :auth_verification, as: :json
       expect(response).to have_http_status(:success)
     end
