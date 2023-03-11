06d7505e34d649ace97c130a02ffdadf2a556a39
DataNick <DataNick@users.noreply.github.com>
2023-03-11 11:16:49 -0500

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :auth_verification]
+      resources :users, only: :create
       get '/auth_verification', to: 'users#auth_verification'
-      post '/auth/login', to: 'authentication#login'
-      delete '/auth/logout', to: 'authentication#logout'
+      post '/login', to: 'authentication#login'
+      delete '/logout', to: 'authentication#logout'
     end
   end
 end
